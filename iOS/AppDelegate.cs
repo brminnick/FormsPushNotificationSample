8a6e8b883a127e7439510701bdbcec9b05a72ef9
brminnick <brandon.r.minnick@gmail.com>
2017-02-06 10:53:25 -0700

    Added Azure Mobile Services Client

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,22 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿using UIKit;
 using Foundation;
-using UIKit;
+
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using UserNotifications;
 
 namespace FormsPushNotificationSample.iOS
 {
 	[Register("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
+		MobileServiceClient Client { get; set; }
+
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
 
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+
 			LoadApplication(new App());
 
 			return base.FinishedLaunching(app, options);
+		}
+
+		void RegisterForPushNotifications()
+		{
+			if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+			{
+				RegiserForPushNotificationsIOS10();
+			}
+			else
+			{
+				RegisterForPushNotificationsIOS9();
+			}
+
+		}
+
+		void RegisterForPushNotificationsIOS9()
+		{
+			var settings = UIUserNotificationSettings.GetSettingsForTypes(UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound, new NSSet());
+			UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+			UIApplication.SharedApplication.RegisterForRemoteNotifications();
+		}
+
+		void RegiserForPushNotificationsIOS10()
+		{
+			UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert, (approved, err) =>
+			{
+				UIApplication.SharedApplication.RegisterForRemoteNotifications();
+			});
 		}
 	}
 }
